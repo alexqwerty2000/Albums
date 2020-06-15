d56fb2a2c766fe830e0359f7655b77d7f8d638d3
alexqwerty2000 <ivanov.alex1@gmail.com>
2020-06-15 16:45:47 +0300

    Created albums&card&cardsection components

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View,Text,StyleSheet} from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = ({album}) => {
     return (
         <View>
             <Text>{album.title}</Text>
+            <Card/>
         </View>
     )
 }
---
+++ src/components/CardSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const CardSection = () => {
+    return(
+    <View style = {styles.cardSection}>
+
+    </View>)
+}
+const styles = {
+    cardSection : {
+        borderBottomWidth: 1,
+    }
+}
+export default CardSection;
